02325d28f1adf1825f6b224a00d557a72a6a584c
dreamk <dreamk@qq.com>
2025-10-06 22:01:39 +0800

    refactor: 优化next-auth类型声明文件，移除未使用的Prisma导入

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import { $Enums, Prisma } from '@prisma/client'
+import { $Enums } from '@prisma/client'
 import type { DefaultSession, DefaultUser } from 'next-auth'
 import { DefaultJWT } from 'next-auth/jwt'
 
